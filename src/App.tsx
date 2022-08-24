e1ba50f6650d3c6ca5ec6c43de0da7b57913ff1e
git4res <akuzin20@gmail.com>
2022-08-24 19:29:11 +0300

    fix to prevent console warnings

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as esbuild from 'esbuild-wasm';
 import React, {useEffect, useReducer, useRef} from 'react';
 import {initialInputState, inputReducer} from "./state/reducers/inputReducer";
 import {SET_CODE, SET_INPUT} from "./state/reducers/inputTypes";
+import {unpkgPathPlugin} from "./plugins/unpkg-path-plugin";
 
 const App = () => {
     const [state, dispatch] = useReducer(inputReducer, initialInputState)
@@ -10,11 +11,18 @@ const App = () => {
     const inputHandler = (inputText: string) => dispatch({type: SET_INPUT, payload: inputText});
     const onClickHandler = async () => {
         if (!refContainer.current) return;
-        const transpileRes = await refContainer.current.transform(state.inputText, {
-            loader: 'jsx',
-            target: 'es2015'
+        const transpileRes = await refContainer.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window'
+            }
         })
-        dispatch({type: SET_CODE, payload: transpileRes.code})
+
+        dispatch({type: SET_CODE, payload: transpileRes.outputFiles[0].text})
     };
 
     const startESBuildService = async () => {
